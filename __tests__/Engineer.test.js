3d5422a147b394f0ad5741a3c48b80f6d8160a45
nbross <nbross2001@gmail.com>
2022-04-28 22:13:15 -0400

    3 failing tests for Engineer

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require('../lib/Engineer');
+
+test('test if we can create a engineer object', () => {
+    const engineer = new Engineer('Nick', 2525, 'nbross2001@gmail.com', 'nbross');
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+test('test to get engineer github value', () => {
+    const engineer = new Engineer('Nick', 2525, 'nbross2001@gmail.com', 'nbross');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('test to get role of employee', () => {
+    const engineer = new Engineer('Nick', 2525, 'nbross2001@gmail.com', 'nbross');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
