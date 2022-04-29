c5a2681fbcb64eced1119bd383711b2865475f6b
nbross <nbross2001@gmail.com>
2022-04-29 16:39:28 -0400

    started question prompts for user

--- index.js
+++ index.js
@@ -8,3 +8,59 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const jest = require('jest');
 const path = require('path');
+
+const promptUser = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your ID?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ('Please enter a valid work ID!')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your email?",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is your github username?",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                }
+            }
+        },
+    ]);
+}
+
+startPromptUser()
