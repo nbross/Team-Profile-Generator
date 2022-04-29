70178da6464e2efc0101c02dfc809efa95c3ac3a
nbross <nbross2001@gmail.com>
2022-04-29 15:47:48 -0400

    added constructors for the classes, tests now pass, and started working on index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// All Classes
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+// All Dependencies
+const fs = require('fs');
+const inquirer = require('inquirer');
+const jest = require('jest');
+const path = require('path');
