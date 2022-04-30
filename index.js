8cee04b808299a507a7df3109a6ab562f0e25943
nbross <nbross2001@gmail.com>
2022-04-29 22:58:01 -0400

    got the rest of the together, tested it in the terminal and all is good

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const buildPage = require('./src/generate-page');
 const teamMembersArray = [];
 
 const promptAddManager = () => {
+    console.log(`
+    =================================================
+    Welcome to the Team Builder!
+    The starting questions will be for the manager.
+    =================================================`)
     return inquirer.prompt ([
         {
             type: 'input',
@@ -69,10 +74,11 @@ const promptAddManager = () => {
         },
     ]).then(promptAnswers => {
         console.log(promptAnswers);
-        const { name,id, email, officeNumber } = managerInput;
+        const { name,id, email, officeNumber } = promptAnswers;
         const manager = new Manager(name, id, email, officeNumber);
 
         teamMembersArray.push(manager);
+        promptChoiceMenu();
     })
 };
 
@@ -99,7 +105,145 @@ const promptChoiceMenu = () => {
 };
 
 const promptAddEngineer = () => {
+    console.log(`
+    =====================
+    Add a New Engineer ;)
+    =====================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name of Engineer? (Required)',
+            validate: engineerName => {
+                if (engineerName) {
+                    return true;
+                } else {
+                    console.log("Please enter engineer's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your work ID? (Required)',
+            validate: id => {
+                if (id) {
+                    return true;
+                } else {
+                    console.log ('Please enter a valid work ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your email?",
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is your github? (Required)",
+            validate: github => {
+                if (github) {
+                    return true;
+                } else {
+                    console.log ('Please enter your github!');
+                    return false;
+                }
+            }
+        },
+    ]).then(promptAnswers => {
+        console.log(promptAnswers);
+        const { engineerName, id, email, github } = promptAnswers;
+        const engineer = new Engineer(engineerName, id, email, github);
 
+        teamMembersArray.push(engineer);
+        promptChoiceMenu();
+    })
+};
+
+const promptAddIntern = () => {
+    console.log(`
+    =====================
+    Add a New Intern ;)
+    =====================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name of Intern? (Required)',
+            validate: internName => {
+                if (internName) {
+                    return true;
+                } else {
+                    console.log("Please enter Intern's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your work ID? (Required)',
+            validate: id => {
+                if (id) {
+                    return true;
+                } else {
+                    console.log ('Please enter a valid work ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your email?",
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What school do you attend? (Required)",
+            validate: school => {
+                if (school) {
+                    return true;
+                } else {
+                    console.log ('Please enter your school!');
+                    return false;
+                }
+            }
+        },
+    ]).then(promptAnswers => {
+        console.log(promptAnswers);
+        const { internName, id, email, school } = promptAnswers;
+        const intern = new Intern(internName, id, email, school);
+
+        teamMembersArray.push(intern);
+        promptChoiceMenu();
+    })
+};
+
+const finishTeam = () => {
+    
 }
 
 promptAddManager()
